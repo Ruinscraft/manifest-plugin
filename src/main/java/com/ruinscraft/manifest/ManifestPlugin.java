e7f3fcb396576269374e1e27f26e3d17c4810a42
Dexton Anderson <contact@dextonanderson.com>
2021-07-27 16:57:39 -0500

    Init

---
+++ src/main/java/com/ruinscraft/manifest/ManifestPlugin.java
@@ -0,0 +1,18 @@
+package com.ruinscraft.manifest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ManifestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+
+}
