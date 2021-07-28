82131e6b5bbf5a7e91a6bc197da898998aa83538
Dexton Anderson <contact@dextonanderson.com>
2021-07-27 21:35:23 -0500

    Implementation

--- src/main/java/com/ruinscraft/manifest/ManifestPlugin.java
+++ src/main/java/com/ruinscraft/manifest/ManifestPlugin.java
@@ -1,18 +1,105 @@
 package com.ruinscraft.manifest;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ManifestPlugin extends JavaPlugin {
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ManifestPlugin extends JavaPlugin implements Listener {
+
+    private Map<String, String> serverModManifest;
+    private Map<Player, Map<String, String>> playerMods;
 
     @Override
     public void onEnable() {
+        serverModManifest = new HashMap<>();
+        playerMods = new HashMap<>();
 
+        updateServerModManifest();
+
+        getLogger().info("Mods manifest:");
+        for (String mod : serverModManifest.keySet()) {
+            getLogger().info(mod + " " + serverModManifest.get(mod));
+        }
+        getLogger().info("We will alert users if they do not have these mods installed and the hashes don't match");
+
+        getServer().getMessenger().registerIncomingPluginChannel(this, "manifest:mods_manifest", (s, player, bytes) -> {
+            ByteArrayDataInput in = ByteStreams.newDataInput(bytes);
+            int size = in.readInt();
+            Map<String, String> mods = new HashMap<>(size);
+            for (int i = 0; i < size; i++) {
+                mods.put(in.readUTF(), in.readUTF());
+            }
+            playerMods.put(player, mods);
+        });
+
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-
+        getServer().getMessenger().unregisterIncomingPluginChannel(this);
+        playerMods.clear();
     }
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        getServer().getScheduler().runTaskLater(this, () -> {
+            if (player.isOnline()) {
+                if (!hasMods(player)) {
+                    player.sendMessage(ChatColor.LIGHT_PURPLE + "You do not have the lastest mods");
+                }
+            }
+        }, 60L);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        playerMods.remove(event.getPlayer());
+    }
+
+    private void updateServerModManifest() {
+        String manifestURL = "https://storage.googleapis.com/stevecinema-us-download/mods/manifest.txt";
+
+        try (Scanner scanner = new Scanner(new URL(manifestURL).openStream())) {
+            while (scanner.hasNext()) {
+                String fileName = scanner.next();
+                String hash = scanner.next();
+                serverModManifest.put(fileName, hash);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean hasMods(Player player) {
+        if (playerMods.containsKey(player)) {
+            Map<String, String> mods = playerMods.get(player);
+
+            for (String serverMod : serverModManifest.keySet()) {
+                if (!(mods.containsKey(serverMod)
+                        && mods.get(serverMod).equals(serverModManifest.get(serverMod)))) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
 
 }
